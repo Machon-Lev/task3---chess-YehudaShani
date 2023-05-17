8ce5030827c78d82cc715e892bc1f0951a24de72
yehuda shani <yehudashani88@gmail.com>
2023-05-17 13:07:56 +0300

    minor tweek

--- src/Queen.cpp
+++ src/Queen.cpp
@@ -3,8 +3,12 @@
 
 bool Queen::validMove(Board board, Location& source, Location& dest)
 {
+	/*
+	* The queen can move like a bishop or a rook.
+	* If the move is valid for either of them, it is valid for the queen.
+	*/
 	Bishop bishop = Bishop(isWhite());
 	Rook rook = Rook(isWhite());
 
-	return bishop.validMove(board, source, dest) || rook.validMove(board, source, dest);
+	return bishop.validMove(board, source, dest) || rook.validMove(board, source, dest); 
 }
